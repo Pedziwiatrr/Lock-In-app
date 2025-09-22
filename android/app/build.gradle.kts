c3511aec3728d2aed098f610614153b9f7eaf197
Pedziwiatrr <michalozaurus@gmail.com>
2025-09-22 02:51:08 +0200

    config version update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "io.github.pedziwiatrr.lockin"
         minSdkVersion(flutter.minSdkVersion)
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
         multiDexEnabled = true
     }
